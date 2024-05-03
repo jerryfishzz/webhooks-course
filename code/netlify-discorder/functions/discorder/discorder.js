50c8fccacbe4678447d95ada47d6e17da1f3ba64
Jerry Zhang <26999653+jerryfishzz@users.noreply.github.com>
2024-05-03 12:19:32 +1200

    Install netlify project
    feat: install netlify project and make some code style modification

--- code/netlify-discorder/functions/discorder/discorder.js
+++ code/netlify-discorder/functions/discorder/discorder.js
@@ -1,12 +1,13 @@
-const axios = require("axios").default;
+const axios = require('axios').default
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+// event is kinda like req
 exports.handler = async (event, context) => {
   try {
-    const body = JSON.parse(event.body);
-    const username = body.sender.login;
-    const avatarUrl = body.sender.avatar_url;
-    const repoName = body.repository.name;
+    const body = JSON.parse(event.body)
+    const username = body.sender.login
+    const avatarUrl = body.sender.avatar_url
+    const repoName = body.repository.name
     const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
       content: `:taco: :taco: :taco: ${username} just starred ${repoName}! :rocket: :muscle: :tada: :taco:`,
       embeds: [
@@ -16,12 +17,12 @@ exports.handler = async (event, context) => {
           },
         },
       ],
-    });
-    console.log("Submitted!");
+    })
+    console.log('Submitted!')
     return {
       statusCode: 204,
-    };
+    }
   } catch (err) {
-    return { statusCode: 500, body: err.toString() };
+    return { statusCode: 500, body: err.toString() }
   }
-};
+}
